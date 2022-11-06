7026fc7d2420efc68faf9124032515fdf58c889b
Adonis Shabani <adonis.shabani18@gmail.com>
2022-11-06 14:35:04 +0100

    edited iam

--- iam.tf
+++ iam.tf
@@ -2,34 +2,32 @@ module "IAM-module" {
   source  = "app.terraform.io/devops-training-ick/IAM-module/assigment"
   version = "1.0.0"
   # insert required variables here
-}
-
-
-user_name   = "adonis-user"
-policy_name = "adonis-policy"
-policy_document = {
-  Version = "2012-10-17"
-  Statement = [
-    {
-      Action = [
-        "ec2:*", "iam:GetAccountPasswordPolicy", "elasticloadbalancing:DescribeLoadBalancers"
-      ]
-      Effect   = "Allow"
-      Resource = "*"
-    },
-    {
-      Action = [
-        "iam:ChangePassword"
-      ]
-      Effect   = "Allow"
-      Resource = "*"
-    },
-    {
-      Action = [
-        "s3:*"
-      ]
-      Effect   = "Allow"
-      Resource = "*"
-    }
-  ]
+  user_name   = "adonis-user"
+  policy_name = "adonis-policy"
+  policy_document = {
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:*", "iam:GetAccountPasswordPolicy", "elasticloadbalancing:DescribeLoadBalancers"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "iam:ChangePassword"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "s3:*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  }
 }
